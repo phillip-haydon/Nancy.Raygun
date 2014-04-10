73150836be4bbcb256dcc6f254cb2e5b95c0928a
Oliver Nixon <oliver.nixon@smartfleetsolutions.com>
2014-04-10 14:53:22 +0100

    Can include the current user in the logged message detail

---
+++ src/Nancy.Raygun.Tests/RaygunMessageBuilderTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Nancy.Raygun.Tests
+{
+    public class RaygunMessageBuilderTests
+    {
+        [Fact]
+        public void Sends_user_identity_when_set()
+        {
+            const string expectedUserIdentity = "a.user@example.com";
+            var message = RaygunMessageBuilder.New
+                                              .SetUser(expectedUserIdentity)
+                                              .Build();
+
+            Assert.Equal(expectedUserIdentity, message.Details.User.Identifier);
+        }
+
+        [Fact]
+        public void Does_not_send_user_identity_when_not_set()
+        {
+            var message = RaygunMessageBuilder.New
+                                              .Build();
+
+            Assert.Null(message.Details.User);
+        }
+
+        [Fact]
+        public void Does_not_send_user_identity_when_set_to_null()
+        {
+            var message = RaygunMessageBuilder.New
+                                              .SetUser(null)
+                                              .Build();
+
+            Assert.Null(message.Details.User);
+        }
+
+        [Fact]
+        public void Does_not_send_user_identity_when_set_to_empty_string()
+        {
+            var message = RaygunMessageBuilder.New
+                                              .SetUser("")
+                                              .Build();
+
+            Assert.Null(message.Details.User);
+        }
+    }
+}
